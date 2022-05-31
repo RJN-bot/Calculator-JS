709769f0badf69bd113ad7254a70783c4a9b9c31
Arjun <arjunsingh8112@gmail.com>
2022-05-31 10:43:45 +0530

    event listeners added for each btn

--- app.js
+++ app.js
@@ -51,19 +51,58 @@ const btn_clear = document.querySelector('.ac');
 const btn_sign = document.querySelector('.sign');
 const btn_perc = document.querySelector('.perc');
 const btn_add = document.querySelector('.add');
-const btn_7 = document.querySelector('.7');
-const btn_8 = document.querySelector('.8');
-const btn_9 = document.querySelector('.9');
+const btn_7 = document.querySelector('.btn_7');
+const btn_8 = document.querySelector('.btn_8');
+const btn_9 = document.querySelector('.btn_9');
 const btn_sub = document.querySelector('.sub');
-const btn_4 = document.querySelector('.4');
-const btn_5 = document.querySelector('.5');
-const btn_6 = document.querySelector('.6');
+const btn_4 = document.querySelector('.btn_4');
+const btn_5 = document.querySelector('.btn_5');
+const btn_6 = document.querySelector('.btn_6');
 const btn_multi = document.querySelector('.multi');
-const btn_1 = document.querySelector('.1');
-const btn_2 = document.querySelector('.2');
-const btn_3 = document.querySelector('.3');
+const btn_1 = document.querySelector('.btn_1');
+const btn_2 = document.querySelector('.btn_2');
+const btn_3 = document.querySelector('.btn_3');
 const btn_divide = document.querySelector('.divide');
 const btn_0 = document.querySelector('.zero');
 const btn_deci = document.querySelector('.deci');
 const btn_equals = document.querySelector('.equals');
 
+
+btn_1.addEventListener('click', () => {
+});
+btn_2.addEventListener('click', () => {
+});
+btn_3.addEventListener('click', () => {
+});
+btn_4.addEventListener('click', () => {
+});
+btn_5.addEventListener('click', () => {
+});
+btn_6.addEventListener('click', () => {
+});
+btn_7.addEventListener('click', () => {
+});
+btn_8.addEventListener('click', () => {
+});
+btn_9.addEventListener('click', () => {
+});
+btn_0.addEventListener('click', () => {
+});
+btn_clear.addEventListener('click', () => {
+});
+btn_sign.addEventListener('click', () => {
+});
+btn_perc.addEventListener('click', () => {
+});
+btn_add.addEventListener('click', () => {
+});
+btn_sub.addEventListener('click', () => {
+});
+btn_multi.addEventListener('click', () => {
+});
+btn_divide.addEventListener('click', () => {
+});
+btn_deci.addEventListener('click', () => {
+});
+btn_equals.addEventListener('click', () => {
+});
